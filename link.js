03a730477c4bec7eb3bf14582dddd382368edac3
sreebaskumar832 <130956652+sreebaskumar832@users.noreply.github.com>
2023-04-26 19:33:38 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=testtoken=https://test.link.com&html_redirect=1 ";
+const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbU9MbHZtUlJoOVFCTEE5aXB2UTJGMmpZNjJiQXxBQ3Jtc0ttVHNGNHBJeUlDNFNCNHlQc3FqUy0tdmFQNkl4TTVWOWJrUFlPQ2JTc1Rsd0FueG81bVNUYmtwS2d4dFI0WFR6c1pNS3JUdnlvUHN3eWZaOTRYcThRMnQzTDRvSm5YblBTWnN0ZWVTMVVuaGxQUnVqMA&q=https%3A%2F%2Fxblroo.paiatialdates.net%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D187579%26s2%3D1785767%26j1%3D1&html_redirect=1 ";
